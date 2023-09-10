76ce6ad19d2cea0a9a971462c0e20fc33e4c82fb
John Private <jwmp5051@gmail.com>
2023-09-10 23:56:20 +0100

    Refactoring and add docstrings

--- apartments.py
+++ apartments.py
@@ -1,28 +1,45 @@
+"""
+Author: SadSack963
+Version: 1.0
+Date: 10th September 2023
+
+Scrape property details from a webpage using BeautifulSoup.
+"""
+
 from bs4 import BeautifulSoup
-from load_site import get_webpage
 
-URL = "https://www.apartments.com/san-francisco-ca/"
-FILE = "data/apartments.html"
 
-# TODO: Use BeautifulSoup/Requests to scrape all the listings from the apartments.com web address
-soup = BeautifulSoup(markup=get_webpage(url=URL, file=FILE, load_from_file=True), features="html.parser")
-# or using the default values...
-# soup = BeautifulSoup(get_webpage(), "html.parser")
-placard_container = soup.find(id="placardContainer")
-property_information = placard_container.find_all("div", class_="property-information")
+def get_property_details(html: str) -> tuple:
+    """
+    Uses BeautifulSoup to parse a web page and extract property details.
 
-# TODO: Create a list of links for all the listings you scraped
-property_links = [element.find("a", class_="property-link").get("href") for element in property_information]
-# print(property_links)
-# print(len(property_links))
+    :param html: raw HTML of the web page
+    :type html: string
+    :return: tuple containing links, prices and addresses for the properties
+    """
 
-# TODO: Create a list of prices for all the listings you scraped
-top_level_infos = placard_container.find_all("div", class_="top-level-info")
-property_prices = [element.find("p", class_="property-pricing").get_text() for element in top_level_infos]
-# print(property_prices)
-# print(len(property_prices))
+    # TODO: Use BeautifulSoup to scrape all the listings from the web page
+    soup = BeautifulSoup(markup=html, features="html.parser")
 
-# TODO: Create a list of addresses for all the listings you scraped
-property_addresses = [element.find("div", class_="property-address").get("title") for element in property_information]
-# print(property_addresses)
-# print(len(property_addresses))
+    # Find the relevant sections of property information
+    placard_container = soup.find(id="placardContainer")
+    property_information_list = placard_container.find_all(name="div", class_="property-information")
+    top_level_info_list = placard_container.find_all(name="div", class_="top-level-info")
+
+    # TODO: Create a list of links for all the listings you scraped
+    property_links = [element.find(ame="a", class_="property-link").get("href")
+                      for element in property_information_list]
+
+    # TODO: Create a list of prices for all the listings you scraped
+    property_prices = [element.find(ame="p", class_="property-pricing").get_text()
+                       for element in top_level_info_list]
+
+    # TODO: Create a list of addresses for all the listings you scraped
+    property_addresses = [element.find(ame="div", class_="property-address").get("title")
+                          for element in property_information_list]
+
+    return property_links, property_prices, property_addresses
+
+
+if __name__ == "__main__":
+    property_details = get_property_details()
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from load_site import get_webpage
+from apartments import get_property_details
+URL = "https://www.apartments.com/san-francisco-ca/"
+FILE = "data/apartments.html"
 
+# TODO: Use requests to get the web page
+html = get_webpage(url=URL, file=FILE)
 
+# TODO: Use BeautifulSoup to scrape all the listings from the web page
+property_links, property_prices, property_addresses = get_property_details()
